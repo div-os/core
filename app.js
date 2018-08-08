072e6ac3451609028655fa9e8a5b447ba512ff93
Guilherme <super.driver.512@gmail.com>
2018-08-08 03:23:33 -0300

    WIP: Vinyl.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let express = require('express');
 
+let globMiddleware = require('./globMiddleware');
+
 let app = express();
 let port = process.env.PORT || 6000;
+
+globMiddleware.register(app);
 
 app.listen(port);
 console.log(`Listening on port ${port}.`);
