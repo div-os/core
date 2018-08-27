57d5fae0aabfeb5770af75baf776fd467b1f1ff4
Guilherme <super.driver.512@gmail.com>
2018-08-26 23:35:25 -0300

    filesApp: WIP.

--- public/apps/files/app.js
+++ public/apps/files/app.js
@@ -36,18 +36,85 @@
 
       wnd.jr.scope.filesApp = this;
 
+      wnd.classList.add('filesApp');
+
+      let contentBoxEl =
+        wnd.div.wm.stdHeader.getContentBoxEl();
+
+      contentBoxEl.innerHTML = `
+        <div class="filesApp_headerActions">
+          <div class="btn-group">
+            <button class="btn btn-default">
+              <i class="icon icon-left"></i>
+            </button>
+
+            <button class="btn btn-default">
+              <i class="icon icon-right"></i>
+            </button>
+          </div>
+
+          <div class="btn-group">
+            <button class="btn btn-default">
+              <i class="icon icon-home"></i>
+            </button>
+
+            <button class="btn btn-default">
+              <i class="icon icon-cw"></i>
+            </button>
+          </div>
+
+          <div class="btn-group">
+            <button class="btn btn-default">
+              <i class="icon icon-layout"></i>
+            </button>
+
+            <button class="btn btn-default">
+              <i class="icon icon-list"></i>
+            </button>
+          </div>
+        </div>
+      `;
+
       wnd.appendChild(jr.createElement(`
         <div class="window-content">
           <div class="pane-group">
-            <div class="pane-sm sidebar padded-more">
+            <div class="pane-sm sidebar">
+              <div class="nav-group">
+                <div class="nav-group-title">
+                  Personal
+                </div>
+
+                <div class="nav-group-item active">
+                  <i class="icon icon-home"></i>
+                  Home
+                </div>
+              </div>
+
+              <div class="nav-group">
+                <div class="nav-group-title">
+                  Devices
+                </div>
+
+                <div class="nav-group-item">
+                  <i class="icon icon-drive"></i>
+                  File system
+                </div>
+              </div>
             </div>
 
             <div class="pane">
               <div
-                class="filesApp_dirBrowser"
+                class="
+                  filesApp_dirBrowser
+                  filesApp_dirBrowser--grid
+                "
+
                 jr-list="for dirEntry of filesApp.dir.entries"
               >
-                <button class="filesApp_dirEntry">
+                <button class="
+                  filesApp_dirEntry
+                  filesApp_dirEntry--grid
+                ">
                   <div jr-class="
                     filesApp_dirEntry-icon
 
